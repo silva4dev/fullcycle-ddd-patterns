72e950266a7814b162d105cf3708f223619e9563
silva4dev <web4dev.lucas@outlook.com>
2023-12-10 11:54:38 -0300

    test: ensure call ProductRepository to create a new product

--- src/infrastructure/repository/product.repository.spec.ts
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductModel from "../db/sequelize/model/product.model";
+import Product from "../../domain/entity/product";
+import ProductRepository from "./product.repository";
 
 describe("Product repository test", () => {
   let sequileze: Sequelize;
@@ -18,5 +20,26 @@ describe("Product repository test", () => {
 
   afterEach(async () => {
     await sequileze.close();
+  });
+
+  it("should create a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+
+    await productRepository.create(product);
+
+    const productModel = (
+      await ProductModel.findOne({ where: { id: "1" } })
+    ).toJSON();
+
+    expect({
+      id: productModel.id,
+      name: productModel.name,
+      price: productModel.price,
+    }).toStrictEqual({
+      id: "1",
+      name: "Product 1",
+      price: 100,
+    });
   });
 });
