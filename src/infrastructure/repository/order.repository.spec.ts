c01acd8d5b0e0cce149f4f4cfd102fb3804d3285
silva4dev <web4dev.lucas@outlook.com>
2023-12-11 11:53:35 -0300

    test: add order repository

---
+++ src/infrastructure/repository/order.repository.spec.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize-typescript";
+
+describe("Order repository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    await sequelize.addModels([]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+});
