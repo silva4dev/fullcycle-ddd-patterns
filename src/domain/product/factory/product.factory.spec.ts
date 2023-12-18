a028afb24bcb040b02f71ae23957e5bd522dc7c7
silva4dev <web4dev.lucas@outlook.com>
2023-12-17 22:51:30 -0300

    test: ensure calls ProductFactory to create  a product type a or b

--- src/domain/product/factory/product.factory.spec.ts
+++ src/domain/product/factory/product.factory.spec.ts
@@ -8,4 +8,12 @@ describe("Product factory unit test", () => {
     expect(product.price).toBe(1);
     expect(product.constructor.name).toBe("Product");
   });
+
+  it("should create a product type b", () => {
+    const product = ProductFactory.create("b", "Product B", 1);
+    expect(product.id).toBeDefined();
+    expect(product.name).toBe("Product B");
+    expect(product.price).toBe(2);
+    expect(product.constructor.name).toBe("ProductB");
+  });
 });
